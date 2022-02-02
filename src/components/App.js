31cc4e9954b9e67ba9ab8834bfb3cc117fd89fdf
Kisich <94457745+frankmombo@users.noreply.github.com>
2022-02-02 10:04:12 +0300

    progress

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,32 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = { videos: []}
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
-      params :{
-        q: term
-      }
+      params: {
+        q: term,
+      },
     });
-    this.setState({videos: response.data.items})
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video})
+    
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos}/>
-        
+        <VideoDetail video={this.state.selectedVideo}/> 
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
